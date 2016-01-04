d09e22c9df2ade63b20598733aeafa75aca6f9f8
ammark47 <ammark1111@gmail.com>
2016-01-03 20:34:06 -0500

    revert to before ghpages

--- _site/app/js/components/Img.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default class Img extends React.Component {
-	render () {
-			return (<img src={this.props.image} width="282" height="200" />)
-		}
-}
--- _site/app/js/constants/TeamConstants.js
+++
@@ -1,8 +0,0 @@
-var keyMirror = require('keyMirror');
-
-module.exports = keyMirror({
-	RECIEVE_TEAM: null,
-	SET_TEAM: null,
-	ADD_HOVER: null,
-	REMOVE_HOVER: null
-});
